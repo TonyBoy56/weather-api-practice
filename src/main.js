6032f6e0b2ee28386b40505ce7fc3d451bf4ed56
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-30 16:45:07 -0700

    add commented out jQuery code for later implementation.

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import $ from 'jQuery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+// import { WeatherService } from './../src/weather-service.js';
+
+// // .ready is depreciated. Using '$'(jQuery), instead
+// $(document).$(function() {
+
+// })
