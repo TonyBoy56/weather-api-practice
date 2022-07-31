d59573bb4353e3f6ca1a6b83f423d9406d93b904
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-30 17:28:34 -0700

    add a 'response' variable to contain the prased JSON response as a type of 'text', upon an event of it's load ending. Wrap as a function.

--- src/weather-service.js
+++ src/weather-service.js
@@ -3,5 +3,10 @@ export class WeatherService {
   getWeatherByCity() {
       let request = new XMLHttpRequest();
       const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
+
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        
+      })
   }
 }
