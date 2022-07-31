06aade3f05589824ccef047095dfca7bb88042cf
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-30 17:26:23 -0700

    add a the openweathermap api url as 'url'.

--- src/weather-service.js
+++ src/weather-service.js
@@ -2,5 +2,6 @@ export class WeatherService {
   
   getWeatherByCity() {
       let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
   }
 }
