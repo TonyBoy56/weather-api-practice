81f56af2f0ec9cea3e372710c0950590e380edfa
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-31 12:55:18 -0700

    add jQuery logic to handle the API request from weather-service.js.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 // UI LOGIC //
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { WeatherService } from "./../src/weather-service.js";
 
-// import $ from 'jQuery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
-// import { WeatherService } from './../src/weather-service.js';
+// jQuery DOM logic against weather-service API request
 
-// // .ready is depreciated. Using '$'(jQuery), instead
-// $(document).$(function() {
+$(document).ready(function () {
+  $("#weatherLocation").click(function () {
+    const city = $("#location").val("");
 
-// })
+    (async () => {
+      let weatherService = new WeatherService();
+      const response = await weatherService.getWeatherByCity(city);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      $(".showHumdidity").text(
+        `The humidity in ${city} is ${response.main.humidity}%.`
+      );
+      $(".showTemp").text(
+        `The temperature in ${city} is ${response.main.temp} degrees.`);
+    }
+  });
+});
+
+// jQuery AJAX API request and DOM logic
