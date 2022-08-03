598d2b364b644685681cc51d6ff9c812e0d365fa
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-08-03 13:15:17 -0700

    comment out the working implementation of fetching an API request via XMLHttpRequest format. Begin testing with jquery.

--- src/main.js
+++ src/main.js
@@ -4,52 +4,63 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 // import { WeatherService } from "./weather-service";
 
-function getWeatherByCity(city) {
-  ///////////////////////////////////////////utilizing XMLHttpRequest method////////////////////////////////////////
+$(document).ready(function () {
+  $("#hidden").hover(function () {
+    $(this).css("color", "black");
+  },
+  );
+});
+// function getWeatherByCity(city) {
+//   ///////////////////////////////////////////utilizing XMLHttpRequest method////////////////////////////////////////
 
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+//   let request = new XMLHttpRequest();
+//   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
-  request.addEventListener("loadend", function () {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city)
-    }
-  });
+//   request.addEventListener("loadend", function () {
+//     const response = JSON.parse(this.responseText);
+//     if (this.status === 200) {
+//       printElements(response, city);
+//     } else {
+//       printError(this, response, city)
+//     }
+//   });
 
-  request.open("GET", url, true);
-  request.send();
-}
+//   request.open("GET", url, true);
+//   request.send();
+// }
 
-function printElements(apiResponse, city) {
-  document.querySelector(
-    ".showHumidity"
-  ).innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.`;
-  document.querySelector(
-    ".showTemp"
-  ).innerText = `The temperature in Kelvins ${city} is ${apiResponse.main.temp} degrees`;
-}
+// function printElements(apiResponse, city) {
+//   document.querySelector(
+//     ".showHumidity"
+//   ).innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.`;
+//   document.querySelector(
+//     ".showTemp"
+//   ).innerText = `The temperature in Kelvins ${city} is ${apiResponse.main.temp} degrees`;
+// }
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').text = `There was an error in handling your request for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
-}
+// function printError(request, apiResponse, city) {
+//   document.querySelector('#showResponse').text = `There was an error in handling your request for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+// }
 
-function handleFormSubmission() {
-  event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeatherByCity(city);
-}
+// function handleFormSubmission() {
+//   event.preventDefault();
+//   const city = document.querySelector('#location').value;
+//   document.querySelector('#location').value = null;
+//   getWeatherByCity(city);
+// }
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
-})
+// window.addEventListener("load", function() {
+//   document.querySelector('form').addEventListener("submit", handleFormSubmission);
+// });
+
+/////////////////////////////////////////utilizing Fetch API method/////////////////////////////////////////////
 
 // $(document).ready(function () {
+//   event.preventDefault();
 //   $("#weatherLocation").click(function () {
-//     const city = $("#location").val("");
+//     getWeatherByCity(city);
+//     const city = $("#location").val();
+//     $('#location').val("");
 
 //     (async () => {
 //       const response = await getWeatherByCity(city);
@@ -57,12 +68,9 @@ window.addEventListener("load", function() {
 //     })();
 
 //     function getElements(response) {
-//       $(".showHumdidity").text(
-//         `The humidity in ${city} is ${response.main.humidity}%.`
-//       );
-//       $(".showTemp").text(
-//         `The temperature in ${city} is ${response.main.temp} degrees.`
-//       );
+//       return response;
+//       // $(".showHumdidity").innerText = `The humidity in ${city} is ${response.main.humidity}%.`;
+//       // $(".showTemp").innerText = `The temperature in Kelvin in ${city} is ${response.main.temp} degrees.`;
 //     }
 //   });
 // });
